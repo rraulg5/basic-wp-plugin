5e8802c192eef6923debf54d0c31d3ba32d65843
Garcia <rraulg5@gmail.com>
2017-07-14 17:23:55 -0500

    Save Wordpress Post Meta to the Database

--- basic-wp-fields.php
+++ basic-wp-fields.php
@@ -10,14 +10,28 @@ function basicwp_add_custom_metabox() {
 }
 add_action('add_meta_boxes', 'basicwp_add_custom_metabox');
 
-function basicwp_meta_callback() {
+function basicwp_meta_callback($post) {
+	wp_nonce_field(basename(__FILE__), 'basicwp_jobs_nonce');
+	$basicwp_stored_meta = get_post_meta($post->ID);
 ?>
 	<div>
 		<div class="meta-row">
 			<div class="meta-th">
-				<label for="job-id" class="dwwp-row-title">Job ID</label>
+				<label for="job_id" class="basicwp-row-title">Job ID</label>
 				<div class="meta-td">
-					<input type="text" name="job-id" id="job-id" value="">
+					<input type="text" name="job_id" id="job-id" value="<?php if ( ! empty ( $basicwp_stored_meta['job_id'] ) ) {
+					echo esc_attr( $basicwp_stored_meta['job_id'][0] );
+				} ?>">
+				</div>
+			</div>
+		</div>
+		<div class="meta-row">
+			<div class="meta-th">
+				<label for="date_listed" class="basicwp-row-title">Date Listed</label>
+				<div class="meta-td">
+					<input type="text" name="date_listed" id="date-listed" value="<?php if ( ! empty ( $basicwp_stored_meta['date_listed'] ) ) {
+					echo esc_attr( $basicwp_stored_meta['date_listed'][0] );
+				} ?>">
 				</div>
 			</div>
 		</div>
@@ -40,3 +54,25 @@ function basicwp_meta_callback() {
 	</div>
 <?php
 }
+
+function basicwp_meta_save($post_id) {
+	$is_autosave = wp_is_post_autosave($post_id);
+	$is_revision = wp_is_post_revision($post_id);
+	$is_valid_nonce = ( isset( $_POST[ 'basicwp_jobs_nonce' ] ) && wp_verify_nonce( $_POST[ 'basicwp_jobs_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+    // Exits script depending on save status
+    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+        return;
+    }
+
+    if ( isset( $_POST[ 'job_id' ] ) ) {
+    	update_post_meta( $post_id, 'job_id', sanitize_text_field( $_POST[ 'job_id' ] ) );
+    }
+    if ( isset( $_POST[ 'date_listed' ] ) ) {
+    	update_post_meta( $post_id, 'date_listed', sanitize_text_field( $_POST[ 'date_listed' ] ) );
+    }
+    if ( isset( $_POST[ 'principle_duties' ] ) ) {
+    	update_post_meta( $post_id, 'principle_duties', sanitize_text_field( $_POST[ 'principle_duties' ] ) );
+    }
+}
+add_action('save_post', 'basicwp_meta_save');
