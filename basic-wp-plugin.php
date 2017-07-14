a3079cce1450a26c6e946bacc5e42dbfcd15939f
Garcia <rraulg5@gmail.com>
2017-07-14 15:31:47 -0500

    Custom Post Types

--- basic-wp-plugin.php
+++ basic-wp-plugin.php
@@ -9,19 +9,63 @@
  * License: GPLv2
  */
 
-function basicwp_remove_dashboard_widget() {
-	remove_meta_box('dashboard_primary', 'dashboard', 'side');
+//Exit if accessed directly
+if (! defined('ABSPATH')) {
+	exit;
 }
 
-add_action('wp_dashboard_setup', 'basicwp_remove_dashboard_widget');
+function basicwp_register_post_type() {
 
-function basicwp_add_google_link() {
-	global $wp_admin_bar;
-	$wp_admin_bar->add_menu([
-		'id' => 'google_analytics',
-		'title' => 'Google Analytics',
-		'href' => 'http://google.com/analytics',
-	]);	
+	$singular = 'Job';
+	$plural = 'Jobs';
+
+	$labels = [
+		'name' 					=>	$plural,
+		'singular_name' 		=>	$singular,
+		'add_name' 				=>	'Add New',
+		'add_new_item' 			=>	'Add New ' . $singular,
+		'edit' 					=>	'Edit',
+		'edit_item' 			=>	'Edit ' . $singular,
+		'new_item' 				=>	'New ' . $singular,
+		'view' 					=>	'View ' . $singular,
+		'view_item' 			=>	'View ' . $singular,
+		'search_term' 			=>	'Search ' . $plural,
+		'parent' 				=>	'Parent ' . $singular,
+		'not_found' 			=>	'No ' . $plural . ' found',
+		'not_found_in_trash' 	=>	'No ' . $plural . ' in Trash'
+	];
+
+	$args = [
+		'labels'				=>	$labels,
+		'public'				=>	true,
+		'publicly_queryable'	=>	true,
+		'exclude_from_search'	=>	false,
+		'show_in_nav_menus'		=>	true,
+		'show_ui'				=>	true,
+		'show_in_menu'			=>	true,
+		'show_in_admin_bar'		=>	true,
+		'menu_position'			=>	10,
+		'menu_icon'				=>	'dashicons-businessman',
+		'can_export'			=>	true,
+		'delete_with_user'		=>	false,
+		'hierarchical'			=>	false,
+		'has_archive'			=>	true,
+		'query_var'				=>	true,
+		'capability_type'		=>	'post',
+		'map_meta_cap'			=>	true,
+		'rewrite' 				=>	[
+			'slug' => 'jobs',
+			'with_font' => true,
+			'pages' => true,
+			'feeds' => false
+		],
+		'supports'				=>	[
+			'title',
+			'editor',
+			'author',
+			'custom-fields'
+		],
+	];
+	register_post_type('job', $args);
 }
-
-add_action('wp_before_admin_bar_render', 'basicwp_add_google_link');
+add_action('init', 'basicwp_register_post_type');
