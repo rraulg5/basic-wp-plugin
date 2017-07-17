4ac80c6367c8417d7ca91fe31db09cec539c6f07
Garcia <rraulg5@gmail.com>
2017-07-17 09:22:10 -0500

    Properly Enqueue CSS and JS

--- basic-wp-plugin.php
+++ basic-wp-plugin.php
@@ -18,3 +18,13 @@ if (! defined('ABSPATH')) {
 require ( plugin_dir_path(__FILE__) . 'basic-wp-cpt.php');
 require ( plugin_dir_path(__FILE__) . 'basic-wp-render-admin.php');
 require ( plugin_dir_path(__FILE__) . 'basic-wp-fields.php');
+
+function basicwp_admin_enqueue_scripts() {
+	global $pagenow, $typenow;
+
+	if (($pagenow == 'post.php' || $pagenow == 'post-new.php') && $typenow == 'job') {
+		wp_enqueue_style('basicwp-admin-css', plugins_url('css/admin-jobs.css', __FILE__));
+		wp_enqueue_script('basicwp-admin-js', plugins_url('js/admin-jobs.js', __FILE__), array('jquery', 'jquery-ui-datepicker'), '20170717', true);
+	}
+}
+add_action('admin_enqueue_scripts', 'basicwp_admin_enqueue_scripts');
