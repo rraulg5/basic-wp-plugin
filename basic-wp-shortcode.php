ae155c1d18b62b3b1e5c9400f5551d0a075a6e30
Garcia <rraulg5@gmail.com>
2017-07-19 14:37:28 -0500

    Shortcode - Taxonomy Query

--- basic-wp-shortcode.php
+++ basic-wp-shortcode.php
@@ -8,6 +8,23 @@ function basicwp_sample_shortcode($atts, $content = null) {
 		),
 		$atts
 	);
-	return '<h1>' . $atts['title'] . '<h1>';
+
+	$locations = get_terms('location');
+	$displayList = '';
+
+	if (! empty($locations) && ! is_wp_error($locations)) {
+		$displayList.= '<div id="job-location-list">';
+		$displayList.= '<h4>' . esc_html__($atts['title']) . '</h4>';
+		$displayList.= '<ul>';
+
+		foreach ($locations as $location) {
+			$displayList.= '<li class="job-location"><a href="' . esc_url(get_term_link($location)) . '">' . esc_html__($location->name) . '</a></li>';
+		}
+
+		$displayList.= '</ul>';
+		$displayList.= '</div>';
+	}
+
+	return $displayList;
 }
 add_shortcode('job_listing', 'basicwp_sample_shortcode');
